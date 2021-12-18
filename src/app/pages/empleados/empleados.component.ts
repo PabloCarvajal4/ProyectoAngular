4531b68bfed9281288363f6911b34e5da0979630
PabloCarvajal4 <pablocarvajal128@gmail.com>
2021-12-17 20:42:31 -0600

    Deploy Netlify

--- src/app/pages/empleados/empleados.component.ts
+++ src/app/pages/empleados/empleados.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Empleados } from 'src/app/api/models';
 import { EmpleadosControllerService } from 'src/app/api/services';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-empleados',
@@ -8,15 +10,34 @@ import { EmpleadosControllerService } from 'src/app/api/services';
 })
 export class EmpleadosComponent {
   listOfData: Empleados[] = [];
+  http: any;
+  form!: FormGroup;
+  visible = false;
+  accion:boolean=true;
+  idModificar:string='';
+
   constructor(
-    private empleadosService:EmpleadosControllerService
+    private empleadosService:EmpleadosControllerService,
+    private nzMessageService: NzMessageService,
+    private formBuilder: FormBuilder
   ){}
 
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      dni: [''],
+      nombre: [''],
+      apellido : [''],
+      edad: [null],
+      cargo: ['']
+    });
+  }
   ngOnInit():void{
     this.getData();
+
   }
 
   getData():void{
     this.empleadosService.find().subscribe(data=>this.listOfData=data);
   }
-}
+   
+  }
